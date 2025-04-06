4d0683cc8be65378ea17bbf1dd09da8fe860dcf5
Ojas Sanghi <ojas.sanghi@gmail.com>
2025-04-05 20:34:35 -0700

    format files, experimentation

--- retrain/training/tutorials/unet_model.py
+++ retrain/training/tutorials/unet_model.py
@@ -26,8 +26,7 @@ class ConvBNAct(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.seq = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels,
-                      kernel_size=3, padding=1),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
         )
@@ -51,9 +50,9 @@ class Block(nn.Module):
 
 
 class UNetUp(nn.Module):
-    def __init__(self, down_channels,  right_channels):
+    def __init__(self, down_channels, right_channels):
         super().__init__()
-        self.bottom_up = nn.Upsample(scale_factor=2, mode='nearest')
+        self.bottom_up = nn.Upsample(scale_factor=2, mode="nearest")
         self.conv = nn.Conv2d(down_channels, right_channels, kernel_size=1, stride=1)
 
     def forward(self, left, bottom):
@@ -66,13 +65,17 @@ class UNetUp(nn.Module):
 class Bottleneck(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), dilation=2, padding=2)
+        self.conv = nn.Conv2d(
+            in_channels, out_channels, kernel_size=(3, 3), dilation=2, padding=2
+        )
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=(3, 3), dilation=2, padding=2)
+        self.conv2 = nn.Conv2d(
+            out_channels, out_channels, kernel_size=(3, 3), dilation=2, padding=2
+        )
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.relu2 = nn.ReLU()
-        
+
     def forward(self, x):
         out = self.conv(x)
         out = self.bn(out)
@@ -83,31 +86,25 @@ class Bottleneck(nn.Module):
 
 class UNet(nn.Module):
 
-    def __init__(self, encoder_blocks,  encoder_channels, n_cls):
+    def __init__(self, encoder_blocks, encoder_channels, n_cls):
         self.encoder_channels = encoder_channels
         self.depth = len(self.encoder_channels)
         assert len(encoder_blocks) == self.depth
         super().__init__()
-        
+
         self.encoder_blocks = nn.ModuleList(encoder_blocks)
-        
+
         self.blocks = nn.ModuleList()
         # add bottleneck
-        self.blocks.append(Block(
-            self.encoder_channels[-1],
-            self.encoder_channels[-1]
-        ))
-        
+        self.blocks.append(Block(self.encoder_channels[-1], self.encoder_channels[-1]))
+
         self.ups = nn.ModuleList()
         for i in range(1, self.depth):
             bottom_channels = self.encoder_channels[self.depth - i]
             left_channels = self.encoder_channels[self.depth - i - 1]
             right_channels = left_channels
-            self.ups.append(UNetUp(bottom_channels,  right_channels))
-            self.blocks.append(Block(
-                left_channels + right_channels,
-                right_channels
-            ))
+            self.ups.append(UNetUp(bottom_channels, right_channels))
+            self.blocks.append(Block(left_channels + right_channels, right_channels))
         self.last_conv = nn.Conv2d(encoder_channels[0], n_cls, 1)
         # self.dropout = nn.Dropout2d(p=0.1)
         self.bottle = Bottleneck(512, 512)
@@ -130,11 +127,11 @@ class UNet(nn.Module):
 
 def _get_encoder_blocks(model):
     # last modules (ReLUs) of VGG blocks
-    layers_last_module_names = ['5', '12', '22', '32', '42']
+    layers_last_module_names = ["5", "12", "22", "32", "42"]
     result = []
     cur_block = nn.Sequential()
     for name, child in model.named_children():
-        if name == 'features':
+        if name == "features":
             for name2, child2 in child.named_children():
                 cur_block.add_module(name2, child2)
                 if name2 in layers_last_module_names:
@@ -146,7 +143,7 @@ def _get_encoder_blocks(model):
 
 
 def construct_unet(n_cls, pretrain=False):  # no weights inited
-    model = vgg16_bn(weights='DEFAULT')
+    model = vgg16_bn(weights="DEFAULT")
     encoder_blocks = _get_encoder_blocks(model)
     encoder_channels = [64, 128, 256, 512, 1024]  # vgg16 channels
     # prev_channels = encoder_channels[-1]
