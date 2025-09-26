ba839eb63644a979028aadb808db576f4e2fb4cf
Ojas Sanghi <ojas.sanghi@gmail.com>
2025-09-26 09:06:31 -0700

    set up utils structure

---
+++ src/multi_channel/formatting/create_channeled_directories.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Check if the root folder is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <root_folder>"
+    exit 1
+fi
+
+ROOT_FOLDER=$1
+
+# Create the directory structure under the specified root folder
+mkdir -p "$ROOT_FOLDER/ann/all"
+mkdir -p "$ROOT_FOLDER/ann/channeled"
+mkdir -p "$ROOT_FOLDER/ann/mirrored_x"
+mkdir -p "$ROOT_FOLDER/ann/mirrored_y"
+mkdir -p "$ROOT_FOLDER/ann/mirrored_xy"
+mkdir -p "$ROOT_FOLDER/ann/train"
+mkdir -p "$ROOT_FOLDER/ann/val"
+
+mkdir -p "$ROOT_FOLDER/checkpoints/"
+
+mkdir -p "$ROOT_FOLDER/img/all"
+mkdir -p "$ROOT_FOLDER/img/original"
+mkdir -p "$ROOT_FOLDER/img/mirrored_x"
+mkdir -p "$ROOT_FOLDER/img/mirrored_xy"
+mkdir -p "$ROOT_FOLDER/img/mirrored_y"
+mkdir -p "$ROOT_FOLDER/img/train"
+mkdir -p "$ROOT_FOLDER/img/val"
+
+echo "Directory structure created successfully in $ROOT_FOLDER!"
