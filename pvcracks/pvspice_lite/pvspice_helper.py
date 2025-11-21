0c250eea48995709ae83b01930e6230a9d748df1
Ojas Sanghi <ojas.sanghi@gmail.com>
2025-11-21 11:45:38 -0700

    formatting and capitalization + image normalization

--- pvcracks/pvspice_lite/pvspice_helper.py
+++ pvcracks/pvspice_lite/pvspice_helper.py
@@ -8,7 +8,7 @@ Helper functions for reading in ELs and IVs.
 """
 
 
-def Read_IV(IVPath, sav_window=5, sav_order =2, samples = 400):
+def read_IV(IVPath, sav_window=5, sav_order=2, samples=400):
     """
     Read an IV curve from a CSV file, rectify it, smooth it, and interpolate.
 
@@ -30,26 +30,27 @@ def Read_IV(IVPath, sav_window=5, sav_order =2, samples = 400):
     Ic : numpy.ndarray
         Current values corresponding to vn after smoothing and interpolation.
     """
-    import pandas as pd
     import numpy as np
-    import scipy
+    import pandas as pd
     import pvlib
-    
-    #Read IV 
+    import scipy
+
+    # Read IV
     IV = pd.read_csv(IVPath)
     # Ensure decreasing signal that is positive
-    Vc,Ic = pvlib.ivtools.utils.rectify_iv_curve(IV['Voltage'],IV['Current'])
+    Vc, Ic = pvlib.ivtools.utils.rectify_iv_curve(IV["Voltage"], IV["Current"])
     # lightly smooth with savgol
-    Vc = scipy.signal.savgol_filter(Vc,sav_window,sav_order)
-    Ic = scipy.signal.savgol_filter(Ic,sav_window,sav_order)
-    
-    vn= np.squeeze(np.linspace(Vc[0],np.max(Vc), samples ))
-    
-    # linear interpret to ensure the generated IV fits the points provided 
-    Ic = np.interp(vn, Vc, Ic)
-    return(vn,Ic)
+    Vc = scipy.signal.savgol_filter(Vc, sav_window, sav_order)
+    Ic = scipy.signal.savgol_filter(Ic, sav_window, sav_order)
 
-def Read_EL(path): 
+    vn = np.squeeze(np.linspace(Vc[0], np.max(Vc), samples))
+
+    # linear interpret to ensure the generated IV fits the points provided
+    Ic = np.interp(vn, Vc, Ic)
+    return (vn, Ic)
+
+
+def read_EL(path):
     """
     Tile individual cell EL images into a single module image.
 
@@ -71,8 +72,9 @@ def Read_EL(path):
     """
     import cv2
 
-    img=cv2.imread(path,cv2.IMREAD_GRAYSCALE)
-    return(img)
+    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    return img
+
 
 def cells2Mod(slice, input_size, rows=6, cols=10):
     """
@@ -94,28 +96,30 @@ def cells2Mod(slice, input_size, rows=6, cols=10):
     img : numpy.ndarray
         Combined module image formed by tiling resized cell images.
     """
-    import numpy as np
-    import cv2
     import os
+
+    import cv2
+    import numpy as np
 
     rc = 0
     cc = 0
-    img = np.zeros((input_size[0]*rows,input_size[1]*cols))
-    
-    for ind, row in slice.iterrows():
-        cell = Read_EL(f"{os.getcwd()}{row.ELPath}")
-        cell = cv2.resize(cell,input_size)
-        
-        img[rc:rc+input_size[0],cc:cc+input_size[1]]=cell
-        
-        cc +=input_size[1]
-        
-        if cc>=img.shape[1]:
-            cc = 0
-            rc +=input_size[0]
-    return(img)
+    img = np.zeros((input_size[0] * rows, input_size[1] * cols))
 
-def Extract_Params(slice):
+    for ind, row in slice.iterrows():
+        cell = read_EL(f"{os.getcwd()}{row.ELPath}")
+        cell = cv2.resize(cell, input_size)
+
+        img[rc : rc + input_size[0], cc : cc + input_size[1]] = cell
+
+        cc += input_size[1]
+
+        if cc >= img.shape[1]:
+            cc = 0
+            rc += input_size[0]
+    return img
+
+
+def extract_params(slice):
     """
     Extract single-diode model parameters from a DataFrame.
 
@@ -137,11 +141,7 @@ def Extract_Params(slice):
     """
     Params = []
     for ind, row in slice.iterrows():
-        Params.append({
-            'Rs'  : row.Rs,
-            'Rsh' : row.Rsh,
-            'I'   : row.I,
-            'Is'  : row.Is,
-            'N'   : row.N
-        })
+        Params.append(
+            {"Rs": row.Rs, "Rsh": row.Rsh, "I": row.I, "Is": row.Is, "N": row.N}
+        )
     return Params
--- pvcracks/pvspice_lite/pvspice_tools.py
+++ pvcracks/pvspice_lite/pvspice_tools.py
@@ -7,10 +7,11 @@ Build pv module netlists, run ngspice and read in output.
 @author: nrjost, bbyford
 """
 
+
 def run_ngspice(ngpsice_path, circuit_file):
     """
-    Run ngspice using path and netlist/circuit    
-    
+    Run ngspice using path and netlist/circuit
+
     Parameters
     ----------
     ngpsice_path : str
@@ -21,7 +22,7 @@ def run_ngspice(ngpsice_path, circuit_file):
     Returns
     -------
     Nothing the output is saved to the path given in module_to_netlist
-    
+
     Notes
     ------
     To install ngspice follow the instructions here:
@@ -29,17 +30,18 @@ def run_ngspice(ngpsice_path, circuit_file):
 
     """
     import subprocess
+
     ngspice_command = [ngpsice_path, "-b", circuit_file]
     try:
-        subprocess.run(ngspice_command, stdout=subprocess.DEVNULL);
+        subprocess.run(ngspice_command, stdout=subprocess.DEVNULL)
     except subprocess.CalledProcessError as e:
         pass
 
-    
+
 def ngpsice_read_voltage_current_modules(path):
     """
     Read the volatage and current from the ngspice output file
-    
+
     Parameters
     ----------
     path : str
@@ -53,7 +55,7 @@ def ngpsice_read_voltage_current_modules(path):
         current values
     df_iv : float, pandas DataFrame
         IV values
-    
+
     Notes
     -------
     If halfcut is true output IV for a halfcut module interconnection, esle for a series interconnection with full size cells
@@ -71,23 +73,23 @@ def ngpsice_read_voltage_current_modules(path):
         lines = output_file.readlines()
         for nl, line in enumerate(lines):
             # print(line)
-            
+
             if flag:
-                if line =='\n':
-                    current.append(float(lines[nl-1]))
-                elif line.split("\t")[0]!='':
-                    voltage.append(float(line.split('\t')[1]))              
-                    
+                if line == "\n":
+                    current.append(float(lines[nl - 1]))
+                elif line.split("\t")[0] != "":
+                    voltage.append(float(line.split("\t")[1]))
+
             if "Values" in line:
-                flag=True    
-                
-                
-    df_iv['V'] = voltage
-    df_iv['I'] = current
-    
+                flag = True
+
+    df_iv["V"] = voltage
+    df_iv["I"] = current
+
     return voltage, current, df_iv
 
-def Create_Cell_NetCode(params, a, b, c):
+
+def create_cell_netcode(params, a, b, c):
     """
     Build and simulate a mini-module SPICE circuit, then return its IV curve.
 
@@ -111,12 +113,13 @@ def Create_Cell_NetCode(params, a, b, c):
     fitted_current : numpy.ndarray
         Interpolated current values corresponding to vn.
     """
-    
-    elementary_diode = f"""X{a} {a} {b} {c}    Cell Rs={params['Rs']} Rp={params['Rsh']} I={params['I']} BV={30} N={params['N']} Is={params['Is']}
+
+    elementary_diode = f"""X{a} {a} {b} {c}    Cell Rs={params["Rs"]} Rp={params["Rsh"]} I={params["I"]} BV={30} N={params["N"]} Is={params["Is"]}
 """
-    return(elementary_diode)
+    return elementary_diode
 
-def MiniMod_Spice(params, file_path, V, V_Step=0.001, file_name ='', spicepath=''):
+
+def minimod_spice(params, file_path, V, V_Step=0.001, file_name="", spicepath=""):
     """
     Build and simulate a mini-module SPICE circuit, then return its IV curve.
 
@@ -140,51 +143,43 @@ def MiniMod_Spice(params, file_path, V, V_Step=0.001, file_name ='', spicepath='
     fitted_current : numpy.ndarray
         Interpolated current values corresponding to vn.
     """
-    import pvlib
     import numpy as np
-    
+    import pvlib
+
     N_s = len(params)
-    Measurment_offset = {'I': 0,
-                         'Is': 0,
-                         'Rs': 0.0035873181014406622,
-                         'Rsh': 0,
-                         'N': 0}
+    Measurment_offset = {"I": 0, "Is": 0, "Rs": 0.0035873181014406622, "Rsh": 0, "N": 0}
     # Define nominal temperature
-    TNOM=25
+    TNOM = 25
     # Crete Cell netlist for series connections
-    elementary_diode_netlist = ''
-    
-        
-    for n in range(N_s-1):
-        Cell_Params=params[n].copy()
-        
-        Cell_Params['I']   -= Measurment_offset['I']
-        Cell_Params['Is']  -= Measurment_offset['Is']
-        Cell_Params['Rs']  -= Measurment_offset['Rs']
-        Cell_Params['Rsh'] -= Measurment_offset['Rsh']
-        Cell_Params['N']   -= Measurment_offset['N']
-        
-        
-        a = n+n+1
-        b = n+n+2
-        c = n+n+3
-        elementary_diode=Create_Cell_NetCode(Cell_Params,a,b,c)
-        
-        elementary_diode_netlist+=elementary_diode
-    if N_s==1:
-        n=0
+    elementary_diode_netlist = ""
+
+    for n in range(N_s - 1):
+        Cell_Params = params[n].copy()
+
+        Cell_Params["I"] -= Measurment_offset["I"]
+        Cell_Params["Is"] -= Measurment_offset["Is"]
+        Cell_Params["Rs"] -= Measurment_offset["Rs"]
+        Cell_Params["Rsh"] -= Measurment_offset["Rsh"]
+        Cell_Params["N"] -= Measurment_offset["N"]
+
+        a = n + n + 1
+        b = n + n + 2
+        c = n + n + 3
+        elementary_diode = create_cell_netcode(Cell_Params, a, b, c)
+
+        elementary_diode_netlist += elementary_diode
+    if N_s == 1:
+        n = 0
     else:
-        n+=1
-    Cell_Params=params[n].copy()
-    
-        
-    
-    a = n+n+1
-    b = n+n+2
+        n += 1
+    Cell_Params = params[n].copy()
+
+    a = n + n + 1
+    b = n + n + 2
     # last node has to end at ground
     c = 0
-    elementary_diode=Create_Cell_NetCode(Cell_Params,a,b,c)
-    elementary_diode_netlist+=elementary_diode
+    elementary_diode = create_cell_netcode(Cell_Params, a, b, c)
+    elementary_diode_netlist += elementary_diode
 
     # Create Circuit Content
     circuit_content = f"""
@@ -198,7 +193,7 @@ V1 1 0 DC 0V
 
 .control
 * set the sweep for the apropriate voltage range in steps of V_Step
-dc V1 {V[0]} {V[-1]+0.1} {V_Step}
+dc V1 {V[0]} {V[-1] + 0.1} {V_Step}
 set filetype=ascii 
 write {file_path}solar_circuit{file_name}.out
 write {file_path}solar_circuit{file_name}.txt
@@ -208,18 +203,19 @@ quit
 """
     with open(f"{file_path}solar_circuit{file_name}.cir", "w") as circuit_file:
         circuit_file.write(circuit_content)
-    
+
     # Simulate and read output file
-    run_ngspice(spicepath , f"{file_path}solar_circuit{file_name}.cir")
-    
-    _, _, df_iv = ngpsice_read_voltage_current_modules(f"{file_path}solar_circuit{file_name}.out");
-    
+    run_ngspice(spicepath, f"{file_path}solar_circuit{file_name}.cir")
+
+    _, _, df_iv = ngpsice_read_voltage_current_modules(
+        f"{file_path}solar_circuit{file_name}.out"
+    )
     # use PVLIB to snip any negative values
-    Vp,Ip = pvlib.ivtools.utils.rectify_iv_curve(df_iv['V'], df_iv['I'])
-    
-    vn= np.squeeze(np.linspace(V[0],np.max(V), len(V) ))
-    
-    # linear interpret to ensure the generated IV fits the points provided 
+    Vp, Ip = pvlib.ivtools.utils.rectify_iv_curve(df_iv["V"], df_iv["I"])
+
+    vn = np.squeeze(np.linspace(V[0], np.max(V), len(V)))
+
+    # linear interpret to ensure the generated IV fits the points provided
     fitted_current = np.interp(vn, Vp, Ip)
-    
-    return(vn, fitted_current)
+
+    return (vn, fitted_current)
