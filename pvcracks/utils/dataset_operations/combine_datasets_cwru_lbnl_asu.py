1e7022bd77cf3fdedb1862a372f8479eaeb4773a
Ojas Sanghi <ojas.sanghi@gmail.com>
2025-12-05 02:28:13 -0700

    fix mask channeling (no empty channel made) and fix train/test + augment  flow

--- pvcracks/utils/dataset_operations/combine_datasets_cwru_lbnl_asu.py
+++ pvcracks/utils/dataset_operations/combine_datasets_cwru_lbnl_asu.py
@@ -10,36 +10,26 @@ import os
 import shutil
 
 # Path to the directory where the original datasets are stored
-dupont = "/Users/ojas/Desktop/saj/SANDIA/pvcracks_data/CWRU_Dupont_Mono/"
-sunedison = "/Users/ojas/Desktop/saj/SANDIA/pvcracks_data/CWRU_SunEdison_Mono/"
-lbnl = "/Users/ojas/Desktop/saj/SANDIA/pvcracks_data/LBNL_Mono_Cells/"
-asu = "/Users/ojas/Desktop/saj/SANDIA/pvcracks_data/ASU_IHDEANE/"
+dupont = "/Users/ojas/Desktop/saj/SANDIA/pvcracks_data/Fresh_LBNL/"
+sunedison = "/Users/ojas/Desktop/saj/SANDIA/pvcracks_data/Fresh_ASU/"
+lbnl = "/Users/ojas/Desktop/saj/SANDIA/pvcracks_data/CWRU_Dupont_Mono/"
+asu = "/Users/ojas/Desktop/saj/SANDIA/pvcracks_data/Fresh_CWRU_SunEdison/"
+paths = [dupont, sunedison, lbnl, asu]
 
 # Path to the directory where the combined dataset will be stored
-combined = "/Users/ojas/Desktop/saj/SANDIA/pvcracks_data/Combined_CWRU_LBNL_ASU/"
+combined = "/Users/ojas/Desktop/saj/SANDIA/pvcracks_data/Channeled_Combined_CWRU_LBNL_ASU_No_Empty_RNE_Revise/"
 
-additives = ["ann/train/", "ann/val/", "img/train/", "img/val/"]
+additives = ["img/original/", "ann_json/"]
+# note: because the "fresh" datasets were saved in an old file strucutre (ann_json instead of ann/json),
+# once this is run we have to manually move over the files to the new structure (ann/json)
+# in the combined folder
 
 # Copy the files from the original datasets to the combined dataset directory
-for additive in additives:
-    for root, dirs, files in os.walk(dupont + additive):
-        for file in files:
-            shutil.copy(os.path.join(root, file), combined + additive)
-
-for additive in additives:
-    for root, dirs, files in os.walk(sunedison + additive):
-        for file in files:
-            shutil.copy(os.path.join(root, file), combined + additive)
-
-for additive in additives:
-    for root, dirs, files in os.walk(lbnl + additive):
-        for file in files:
-            shutil.copy(os.path.join(root, file), combined + additive)
-
-for additive in additives:
-    for root, dirs, files in os.walk(asu + additive):
-        for file in files:
-            shutil.copy(os.path.join(root, file), combined + additive)
+for path in paths:
+    for additive in additives:
+        for root, dirs, files in os.walk(path + additive):
+            for file in files:
+                shutil.copy(os.path.join(root, file), combined + additive)
 
 # Check the number of files in the combined dataset directory
 num_files = len(
